ca5f89f6859f12fc8fb406aa29d647d676a1df72
yatu360 <yatu360@gmail.com>
2021-06-10 00:04:27 +0100

    base program

--- app.py
+++ app.py
@@ -4,12 +4,38 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///eventdb.db'
 db = SQLAlchemy(app)
 
 class Event(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    date_event = db.Column(db.String(200), nullable =False)
+    #date_event = db.Column(db.String(200), nullable =False)
     Event_name = db.Column(db.String(200), nullable =False)
+    #tickets = db.Column(db.Integer, nullable = False)
+    #redeemed = db.Column(db.String(200))
+    #description = db.Column(db.String(500))
     
+def __repr__(self):
+    return '<Event %r>' %self.id
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        event_name = request.form['content_name']
+        #event_tickets = request.form['content_ticket']
+        new_event = Event(Event_name=event_name)
+
+        try:
+            db.session.add(new_event)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        events = Event.query.order_by(Event.date_created).all()
+        return render_template('index.html', events=events)
+
+if __name__=="__main__":
+    app.run(debug=True)
